05ed9f80cf3bb283aec5be0c5f6fa09bfa5adb31
Drozdova Anastasia <Anastasia.Drozdova@infotecs.ru>
2019-01-11 23:13:44 +0300

    Доработки метода заполнения даты и логина для тестирования контактов. Доработаны методы заполнения формы и логина для групп.

--- test_add_contact.py
+++ test_add_contact.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 import unittest
 from contact import Contact
 from date import Date
+# import os
 
 
 class TestAddContact(unittest.TestCase):
@@ -15,19 +16,17 @@ class TestAddContact(unittest.TestCase):
         wd.get("http://localhost:8080/addressbook/")
 
     def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").submit()
 
-    def fill_date(self, day, month, year, wd, day_locator, month_locator, year_locator):
-        wd.find_element_by_name(day_locator).click()
-        Select(wd.find_element_by_name(day_locator)).select_by_visible_text(day)
-        wd.find_element_by_name(month_locator).click()
-        Select(wd.find_element_by_name(month_locator)).select_by_visible_text(month)
-        wd.find_element_by_name(year_locator).send_keys(year)
+    def fill_date(self, wd, date, day_locator, month_locator, year_locator):
+        if len(date.day) > 0 and len(date.month) > 0:
+            wd.find_element_by_name(day_locator).click()
+            Select(wd.find_element_by_name(day_locator)).select_by_visible_text(date.day)
+            wd.find_element_by_name(month_locator).click()
+            Select(wd.find_element_by_name(month_locator)).select_by_visible_text(date.month)
+        wd.find_element_by_name(year_locator).send_keys(date.year)
 
     def return_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
@@ -43,6 +42,9 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("middlename").send_keys(contact.middle_name)
         wd.find_element_by_name("lastname").send_keys(contact.last_name)
         wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        # wd.find_element_by_name("photo").click()
+        # wd.find_element_by_name("photo").clear()
+        # wd.find_element_by_id("IdOfInputTypeFile").send_keys(os.getcwd() + "/photo.txt")
         wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").send_keys(contact.address)
@@ -54,12 +56,8 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("email2").send_keys(contact.email2)
         wd.find_element_by_name("email3").send_keys(contact.email3)
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        if len(contact.bdate.day) > 0 and len(contact.bdate.month) > 0 and len(contact.bdate.year) > 0:
-            self.fill_date(contact.bdate.day, contact.bdate.month, contact.bdate.year, wd,
-                           day_locator="bday", month_locator="bmonth", year_locator="byear")
-        if len(contact.adate.day) > 0 and len(contact.adate.month) > 0 and len(contact.adate.year) > 0:
-            self.fill_date(contact.adate.day, contact.adate.month, contact.adate.year, wd,
-                           day_locator="aday", month_locator="amonth", year_locator="ayear")
+        self.fill_date(wd, contact.bdate, day_locator="bday", month_locator="bmonth", year_locator="byear")
+        self.fill_date(wd, contact.adate, day_locator="aday", month_locator="amonth", year_locator="ayear")
         wd.find_element_by_name("address2").send_keys(contact.address2)
         wd.find_element_by_name("phone2").send_keys(contact.home_phone2)
         wd.find_element_by_name("notes").send_keys(contact.notes)
--- test_add_group.py
+++ test_add_group.py
@@ -13,10 +13,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost:8080/addressbook/")
 
     def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").submit()
 
@@ -27,12 +24,8 @@ class TestAddGroup(unittest.TestCase):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
