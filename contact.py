c39708b3801174f5121403202ae47fe635ef9879
Drozdova Anastasia <Anastasia.Drozdova@infotecs.ru>
2019-01-14 13:19:22 +0300

    Добавление метода загруки фото

--- contact.py
+++ contact.py
@@ -2,7 +2,7 @@ class Contact:
 
     def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_phone,
                  mobile_phone, work_phone, fax, email, email2, email3, homepage, bdate, adate, address2, home_phone2,
-                 notes):
+                 notes, file_name):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
@@ -23,3 +23,4 @@ class Contact:
         self.address2 = address2
         self.home_phone2 = home_phone2
         self.notes = notes
+        self.file_name = file_name
--- test_add_contact.py
+++ test_add_contact.py
@@ -4,13 +4,41 @@ from selenium.webdriver.support.ui import Select
 import unittest
 from contact import Contact
 from date import Date
-# import os
+import os
 
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+
+    def create_contact(self, wd, contact):
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        self.upload_photo(wd, contact.file_name)
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        self.fill_date(wd, contact.bdate, day_locator="bday", month_locator="bmonth", year_locator="byear")
+        self.fill_date(wd, contact.adate, day_locator="aday", month_locator="amonth", year_locator="ayear")
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_name("phone2").send_keys(contact.home_phone2)
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
 
     def open_home_page(self, wd):
         wd.get("http://localhost:8080/addressbook/")
@@ -34,35 +62,10 @@ class TestAddContact(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_contact(self, wd, contact):
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # wd.find_element_by_name("photo").click()
-        # wd.find_element_by_name("photo").clear()
-        # wd.find_element_by_id("IdOfInputTypeFile").send_keys(os.getcwd() + "/photo.txt")
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        self.fill_date(wd, contact.bdate, day_locator="bday", month_locator="bmonth", year_locator="byear")
-        self.fill_date(wd, contact.adate, day_locator="aday", month_locator="amonth", year_locator="ayear")
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").send_keys(contact.home_phone2)
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
+    def upload_photo(self, wd, file_name):
+        if len(file_name) > 0:
+            wd.find_element_by_name("photo").click()
+            wd.find_element_by_name("photo").send_keys(os.getcwd() + "/" + file_name)
 
     def test_add_contact(self):
         wd = self.wd
@@ -73,7 +76,7 @@ class TestAddContact(unittest.TestCase):
                                         work_phone="abc", fax="abc", email="abc", email2="abc", email3="abc",
                                         homepage="abc", bdate=Date("1", "January", "1990"),
                                         adate=Date("10", "May", "1999"), address2="abc", home_phone2="abc",
-                                        notes="abc"))
+                                        notes="abc", file_name="photo.png"))
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -84,7 +87,7 @@ class TestAddContact(unittest.TestCase):
         self.create_contact(wd, Contact(first_name="", middle_name="", last_name="", nickname="", title="", company="",
                                         address="", home_phone="", mobile_phone="", work_phone="", fax="", email="",
                                         email2="", email3="", homepage="", bdate=Date("", "", ""),
-                                        adate=Date("", "", ""), address2="", home_phone2="", notes=""))
+                                        adate=Date("", "", ""), address2="", home_phone2="", notes="", file_name=""))
 
         self.return_home_page(wd)
         self.logout(wd)
