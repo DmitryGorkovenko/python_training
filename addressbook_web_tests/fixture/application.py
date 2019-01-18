ae9d9264a2a4f551885de75f031cda11ba89ff1b
Drozdova Anastasia <Anastasia.Drozdova@infotecs.ru>
2019-01-18 17:15:51 +0300

    Выделин класс для управления сессиями

--- addressbook_web_tests/fixture/application.py
+++ addressbook_web_tests/fixture/application.py
@@ -3,23 +3,19 @@ from selenium.webdriver.support.ui import Select
 from pathlib import Path
 import os
 
+from addressbook_web_tests.fixture.session import SessionHelper
+
 
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").submit()
 
     def open_groups_page(self):
         wd = self.wd
@@ -41,10 +37,6 @@ class Application:
     def return_to_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def create_contact(self, contact):
         wd = self.wd
--- addressbook_web_tests/test/test_add_contact.py
+++ addressbook_web_tests/test/test_add_contact.py
@@ -13,20 +13,20 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="abc", middle_name="abc", last_name="abc", nickname="abc",
                                title="abc", company="abc", address="abc", home_phone="abc", mobile_phone="abc",
                                work_phone="abc", fax="abc", email="abc", email2="abc", email3="abc",
                                homepage="abc", bdate=Date("1", "January", "1990"),
                                adate=Date("10", "May", "1999"), address2="abc", home_phone2="abc",
                                notes="abc", file_name="photo.jpg"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="", middle_name="", last_name="", nickname="", title="", company="",
                                address="", home_phone="", mobile_phone="", work_phone="", fax="", email="",
                                email2="", email3="", homepage="", bdate=Date(None, None, ""),
                                adate=Date(None, None, ""), address2="", home_phone2="", notes="", file_name=""))
-    app.logout()
+    app.session.logout()
